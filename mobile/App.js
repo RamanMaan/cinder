49a8e92059546d2f69f4fa65b9182ed906bbbed8
Raman Maan <raman_maan@hotmail.com>
2018-02-01 23:42:32 -0600

    Updating folder structure for mobile application

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './containers/home';
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Test test!!!</Text>
-      </View>
+      <Home />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
