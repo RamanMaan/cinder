d6aa5c7e7fe7b3e353953f790096ef73df2a03a6
Raman Maan <maanr@myumanitoba.ca>
2018-02-11 15:44:13 -0600

    Updating tests

--- client/src/containers/Matches.test.jsx
+++ client/src/containers/Matches.test.jsx
@@ -3,6 +3,7 @@ import { shallow, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
 import sinon from 'sinon';
+import fetchMock from 'fetch-mock';
 
 import App from './App';
 import Matches from './Matches';
@@ -10,23 +11,53 @@ import MatchesList from '../components/MatchesList';
 import UserDetail from '../components/UserDetail';
 
 describe('<Matches />', () => {
+  const testMatches = [
+    {
+      userName: 'Some Name 1',
+      userID: 1,
+      matchDate: '2018-02-03T18:09:05.000Z',
+      primaryPic: null
+    },
+    {
+      userName: 'Some Name 2',
+      userID: 5,
+      matchDate: '2018-01-03T18:09:05.000Z',
+      primaryPic: 'http://c.min.ms/m/b/13/13022/e8250eba.jpeg'
+    },
+    {
+      userName: 'Some Name 3',
+      userID: 3,
+      matchDate: '2018-02-06T18:09:05.000Z',
+      primaryPic: null
+    },
+    {
+      userName: 'Some Name 4',
+      userID: 2,
+      matchDate: '2018-02-04T18:09:05.000Z',
+      primaryPic: 'http://c.min.ms/m/b/13/13022/e8250eba.jpeg'
+    }
+  ];
+
+  beforeEach(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+    fetchMock.getOnce(/\/api\/users\/[0-9]*\/matches/, testMatches);
+  });
+
+  afterAll(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+  });
+
   it('renders without crashing', () => {
     // simple smoke test
-    shallow(
-      <MemoryRouter>
-        <Matches />
-      </MemoryRouter>
-    );
+    shallow(<Matches />);
+    mount(<Matches />);
   });
 
   it('renders correctly', () => {
-    const component = renderer
-      .create(
-        <MemoryRouter>
-          <Matches />
-        </MemoryRouter>
-      )
-      .toJSON();
+    const component = renderer.create(<Matches />).toJSON();
+
     expect(component).toMatchSnapshot();
   });
 
