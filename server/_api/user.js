6d3ee4254158524c9d208faa154805deb26c43b5
Raman Maan <maanr@myumanitoba.ca>
2018-02-11 16:17:56 -0600

    Updating query for potential matches

--- server/_api/user.js
+++ server/_api/user.js
@@ -43,7 +43,7 @@ router.get('/:userID', (req, res) => {
     .then(rows => res.status(200).json(rows))
     .catch((err) => {
       console.error(err);
-      res.status(500);
+      return res.status(500);
     });
 });
 
@@ -55,14 +55,27 @@ router.get('/:userID/potentials', (req, res) => {
 
   return mysql.createConnection(MYSQLDB)
     .then((conn) => {
-      const rows = conn.query('SELECT * FROM UsersInfo WHERE UserID != ?', [userID]);
+      const query = mysql.format(`
+      SELECT
+        UInfo.UserID as userID,
+        UInfo.UserName as userName
+      FROM UsersInfo UInfo
+        LEFT JOIN Likes
+          ON (Likes.User1ID = ? AND UInfo.UserID = Likes.User2ID)
+            OR (Likes.User2ID = ? AND UInfo.UserID = Likes.User1ID AND (Likes.UserAction = 0))
+      WHERE
+        Likes.User1ID IS NULL
+        AND UInfo.UserID != ?
+     `, [userID, userID, userID]);
+
+      const rows = conn.query(query);
       conn.end();
       return rows;
     })
     .then(rows => res.status(200).json(rows))
     .catch((err) => {
       console.error(err);
-      res.status(500);
+      return res.status(500);
     });
 });
 
@@ -74,38 +87,37 @@ router.post('/login', (req, res) => {
 router.get('/:userID/matches', (req, res) => {
   const { userID } = req.params;
   if (!userID.match(ID_REGEX)) {
-      return res.status(400).json({ response: 'Invalid user ID' });
+    return res.status(400).json({ response: 'Invalid user ID' });
   }
 
   return mysql.createConnection(MYSQLDB)
-      .then((conn) => {
-          const rows = conn.query(
-              'SELECT \
-                  UI.UserName AS userName, \
-                  UM2.UserID AS userID, \
-                  M.MatchDate AS matchDate, \
-                  UP.PicturePath AS primaryPic \
-              FROM UserMatches UM1 \
-                  INNER JOIN UserMatches UM2 \
-                      ON UM1.MatchID = UM2.MatchID \
-                  INNER JOIN Matches M \
-                      ON UM2.MatchID = M.MatchID \
-                  INNER JOIN UsersInfo UI \
-                      ON UM2.UserID = UI.UserID \
-                  LEFT JOIN UserPicture UP \
-                      ON UM2.UserID = UP.UserID \
-                      AND UP.PrimaryPicture \
-              WHERE \
-                  UM1.UserID != UM2.UserID \
-                  AND UM1.UserID = ? \
-              ORDER BY \
-                  M.MatchDate', [userID]);
+    .then((conn) => {
+      const rows = conn.query(`SELECT
+                  UI.UserName AS userName,
+                  UM2.UserID AS userID,
+                  M.MatchDate AS matchDate,
+                  UP.PicturePath AS primaryPic
+              FROM UserMatches UM1
+                  INNER JOIN UserMatches UM2
+                      ON UM1.MatchID = UM2.MatchID
+                  INNER JOIN Matches M
+                      ON UM2.MatchID = M.MatchID
+                  INNER JOIN UsersInfo UI
+                      ON UM2.UserID = UI.UserID
+                  LEFT JOIN UserPicture UP
+                      ON UM2.UserID = UP.UserID
+                      AND UP.PrimaryPicture
+              WHERE
+                  UM1.UserID != UM2.UserID
+                  AND UM1.UserID = ?
+              ORDER BY
+                  M.MatchDate`, [userID]);
 
-          conn.end();
-          return rows;
-      })
-      .then(rows => res.status(200).json(rows))
-      .catch(err => console.error(err));
+      conn.end();
+      return rows;
+    })
+    .then(rows => res.status(200).json(rows))
+    .catch(err => console.error(err));
 });
 
 module.exports = router;
