1058b9aeedce0bc1572fc08bb4774fccf1d2d573
Fujie Chen <imaple7@gmail.com>
2018-02-10 18:31:51 -0600

    Create rough version of UserDetail

--- client/src/components/UserDetail.jsx
+++ client/src/components/UserDetail.jsx
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import Lightbox from 'react-images';
+
+import './styles/UserDetail.css';
 
 export default class UserDetail extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      genderPic: this.getGenderPic()
+    };
+  }
+
+  getGenderPic() {
+    if (this.props.userDetail.userGender == 'Male')
+      return 'https://image.freepik.com/free-icon/male-gender-symbol_318-32483.jpg';
+    else if (this.props.userDetail.userGender == 'Female')
+      return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqMOagH0yYRUxgDxkRXl_oO9pH_ADsYLmOQ-lhrLlEZCmG4EjG4g';
+    else return 'https://image.flaticon.com/icons/svg/14/14869.svg';
+  }
+
   render() {
     return (
-      <div>
+      <div className="UserDetail">
+        <img className="userImage" src={this.props.userDetail.userPics} />
+        <h2>{this.props.userDetail.userName}</h2>
+        <div className="userAgeWithGender">
+          <span className="userAge">{this.props.userDetail.userAge}</span>
+          <img className="userGender" src={this.state.genderPic} />
+        </div>
         <div>
-          <Lightbox
-            images={[
-              { src: 'http://example.com/img1.jpg' },
-              { src: 'http://example.com/img2.jpg' }
-            ]}
-            isOpen={this.state.lightboxIsOpen}
-            onClickPrev={this.gotoPrevious}
-            onClickNext={this.gotoNext}
-            onClose={this.closeLightbox}
-          />
+          <p>
+            Matched: {this.props.userDetail.matchTime.toLocaleDateString()}{' '}
+            {this.props.userDetail.matchTime.toLocaleTimeString()}
+          </p>
+        </div>
+        <div>
+          <p>Bio: {this.props.userDetail.userBio}</p>
         </div>
 
         <div>
-          <h2> </h2>
+          <p>Note: {this.props.userDetail.matchNote}</p>
         </div>
       </div>
     );
