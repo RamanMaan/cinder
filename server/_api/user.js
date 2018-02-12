bf5ac5b383ce0cf20da0e240e99b252512a52d62
Fujie Chen <imaple7@gmail.com>
2018-02-12 01:53:12 -0600

    Add endpoint to approve/reject potential match user

--- server/_api/user.js
+++ server/_api/user.js
@@ -16,6 +16,12 @@ const MYSQLDB = {
 
 const ID_REGEX = /^[0-9]*$/;
 
+const MATCH_ACTION_REGEX = /(?i:like|pass)/;
+const MATCH_ACTION_LIKE_REGEX = /like/i;
+
+const MATCH_FOUND_JSON = '{"match":true}';
+const MATCH_NOT_FOUND_JSON = '{"match":false}';
+
 router.get('/', (req, res) => {
   mysql.createConnection(MYSQLDB)
     .then((conn) => {
@@ -86,6 +92,58 @@ router.get('/:userID/potentials', (req, res) => {
       console.error(err);
       return res.status(500);
     });
+});
+
+router.get('/:userID/matches/:matchUserID/:result', (req, res) => {
+  const { userID } = req.params.userID;
+  const { matchUserID } = req.params.matchUserID;
+  const { result } = req.params.result;
+
+  if (!userID.match(ID_REGEX)) {
+    return res.status(400).json({ response: 'Invalid user ID' });
+  }
+  if (!matchUserID.match(ID_REGEX)) {
+    return res.status(400).json({ response: 'Invalid match user ID' });
+  }
+  if (!result.match(MATCH_ACTION_REGEX)) {
+    return res.status(400).json({ response: 'Invalid match action' });
+  }
+
+  const userAction = result.match(MATCH_ACTION_LIKE_REGEX) ? 'L' : 'P';
+
+  return mysql.createConnection(MYSQLDB)
+    .then((conn) => {
+      const insert = mysql.format(`
+      INSERT INTO Like (User1ID, User2ID, UserAction)
+      VALUES (?, ?, ?);
+      `, [userID, matchUserID, userAction]);
+
+      conn.query(insert)
+
+      return conn;
+    })
+    .then((conn) => {
+      const checkMatchQuery = mysql.format(`
+      SELECT * 
+      FROM Likes L1 
+          INNER JOIN Likes L2 
+              ON L1.User2ID = L2.User1ID 
+      WHERE 
+          L1.User1ID =  ? 
+          AND L2.User2ID =  ? 
+          AND L1.UserAction = 'L' 
+          AND L2.UserAction = 'L';
+      `, [userID, matchUserID]);
+
+      const isMatched = conn.query(query);
+      conn.end();
+      isMatched ? res.status(200).json(MATCH_FOUND_JSON) : res.status(200).json(MATCH_NOT_FOUND_JSON);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500);
+    });
+
 });
 
 router.post('/login', (req, res) => {
