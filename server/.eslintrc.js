ea66f57503f1a599b6a4e451f8916486d7558390
Raman Maan <maanr@myumanitoba.ca>
2018-02-15 00:17:05 -0600

    Removing AirBNB eslint rules - too much

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,10 +1,42 @@
 module.exports = {
-    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
     "rules": {
-        "max-len": [1, 120, 2, {ignoreComments: true}],
-        "quote-props": [1, "consistent-as-needed"],
-        "no-cond-assign": [2, "except-parens"],
-        "no-param-reassign": 0,
-        "eol-last": ["error", "always"]
+        "max-len": [1, 120, 2, {
+            ignoreComments: true
+        }],
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "warn",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "eqeqeq": [
+            "warn",
+            "smart"
+        ],
+        "no-trailing-spaces": [
+            "warn"
+        ],
+        "eol-last": [
+            "error", "always"
+        ]
     }
 };
--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,3 +1,48 @@
 module.exports = {
-  extends: 'airbnb'
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "max-len": [1, 120, 2, {
+            ignoreComments: true
+        }],
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "warn",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "eqeqeq": [
+            "warn",
+            "smart"
+        ],
+        "no-trailing-spaces": [
+            "warn"
+        ],
+        "eol-last": [
+            "error", "always"
+        ]
+    }
 };
