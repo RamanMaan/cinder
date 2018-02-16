e436f0d4872b4c83894b5fe50f35dcea55e04bd9
Raman Maan <maanr@myumanitoba.ca>
2018-02-16 12:43:02 -0600

    Updating tests

--- web/src/containers/Home.jsx
+++ web/src/containers/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
 import './styles/Home.css';
 import Recommendation from './Recommendation';
