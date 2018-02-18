35df36392d568bf07b635b6d24207d91e9c3673a
Raman Maan <maanr@myumanitoba.ca>
2018-02-18 16:37:17 -0600

    Configuring redux, updating react router to use redux

---
+++ web/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import thunk from 'redux-thunk';
+import createHistory from 'history/createBrowserHistory';
+import rootReducer from './reducers';
+
+export const history = createHistory();
+
+const initialState = {};
+const enhancers = [];
+const middleware = [thunk, routerMiddleware(history)];
+
+const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
+const store = createStore(rootReducer, initialState, composedEnhancers);
+
+export default store;
