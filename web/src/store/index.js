394cf880c77c13cfaecd9426f4f88b74433ddef3
Raman Maan <maanr@myumanitoba.ca>
2018-02-18 22:11:31 -0600

    Added logging on state change

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -6,9 +6,20 @@ import rootReducer from '../reducers';
 
 export const history = createHistory();
 
+function logger({ getState }) {
+  return next => action => {
+    console.group(action.type);
+    console.info('dispatching:', action);
+    const result = next(action);
+    console.log('next state:', getState());
+    console.groupEnd();
+    return result;
+  };
+}
+
 const initialState = {};
 const enhancers = [];
-const middleware = [thunk, routerMiddleware(history)];
+const middleware = [thunk, routerMiddleware(history), logger];
 
 const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
 const store = createStore(rootReducer, initialState, composedEnhancers);
