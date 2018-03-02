fec05fb6da660fbe4959f1a5c2764aa39cdc2efb
Raman Maan <maanr@myumanitoba.ca>
2018-03-01 20:42:17 -0600

    Adding basic home page implementation with navigation

--- mobile/src/containers/home.js
+++ mobile/src/containers/home.js
@@ -1,7 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Icon } from 'native-base';
+
+import cinder from '../theme/variables/cinder';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  itemRight: {
+    marginRight: cinder.iconMarginSize,
+  },
+  itemLeft: {
+    marginLeft: cinder.iconMarginSize,
+  },
+});
 
 export default class Home extends React.Component {
+  static navigationOptions({ navigation }) {
+    const leftItem = (
+      <View style={styles.itemLeft}>
+        <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+          <Icon name="ios-contact" />
+        </TouchableOpacity>
+      </View>
+    );
+    const rightItem = (
+      <View style={styles.itemRight}>
+        <TouchableOpacity onPress={() => navigation.navigate('Matches')}>
+          <Icon name="ios-send" />
+        </TouchableOpacity>
+      </View>
+    );
+
+    return {
+      headerLeft: (
+        leftItem
+      ),
+      headerRight: (
+        rightItem
+      ),
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -11,12 +55,3 @@ export default class Home extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
