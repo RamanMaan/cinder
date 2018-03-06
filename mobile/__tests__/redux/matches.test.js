2dde67cd773c0ae10dd7f9e11892ea14c44768c8
Raman Maan <maanr@myumanitoba.ca>
2018-03-05 22:44:05 -0600

    Updating redux tests

--- mobile/__tests__/redux/matches.test.js
+++ mobile/__tests__/redux/matches.test.js
@@ -1,17 +1,27 @@
 import mockStore from '../_config/mockStore';
 
-import { matchesFetchData, matchesLoading, matchesErrored, matchesFetchSuccess } from '../../src/actions/matches';
-import { matches, matchesHasErrored, matchesIsLoading } from '../../src/reducers/matches';
+import {
+  matchesFetchData,
+  matchesLoading,
+  matchesErrored,
+  matchesFetchSuccess,
+  usersMatched,
+} from '../../src/actions/matches';
+
+import {
+  matches,
+  matchesHasErrored,
+  matchesIsLoading,
+} from '../../src/reducers/matches';
 
 const initialState = {
   loading: false,
   error: false,
   matches: [],
+  matched: false,
 };
 
 describe('Reducers::Matches', () => {
-  const store = mockStore(initialState);
-
   it('should respond to loading action', () => {
     expect(matchesIsLoading(initialState, matchesLoading(true))).toEqual(true);
     expect(matchesIsLoading(initialState, matchesLoading(false))).toEqual(false);
@@ -47,6 +57,22 @@ describe('Actions::Matches', () => {
   it('should handle fetching data - getting an error', async () => {
     fetch.mockResponseFailure(new Error('I am a bad bad error'));
     await store.dispatch(matchesFetchData());
+
+    expect(store.getActions()).toMatchSnapshot();
+  });
+
+  it('should handle the result of user submissions - did match', async () => {
+    const response = '{"data": []}';
+    fetch.mockResponseSuccess(response);
+    await store.dispatch(usersMatched(true));
+
+    expect(store.getActions()).toMatchSnapshot();
+  });
+
+  it('should handle the result of user submissions - did not match', async () => {
+    const response = '{"data": []}';
+    fetch.mockResponseSuccess(response);
+    await store.dispatch(usersMatched(false));
 
     expect(store.getActions()).toMatchSnapshot();
   });
--- mobile/env.js
+++ mobile/env.js
@@ -1,12 +1,12 @@
 
-// TEMPORARY: 
-// Hard code the server's local IP here for the time being while we figure out 
+// TEMPORARY:
+// Hard code the server's local IP here for the time being while we figure out
 // the correct way of setting up environment variables for React Native. It should be
 // displayed on the terminal where your server is running.
 //
 // Example: serverURL = `http://192.168.0.6:5000`;
 //
-export const serverURL = `http://192.168.0.6:5000`;
+export const serverURL = 'http://192.168.0.12:5000';
 
 // temporarily until auth is set up
 export const loggedInUser = 3;
