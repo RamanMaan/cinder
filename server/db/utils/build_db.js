6b34db087cb708cb6257df65d1dd9531a3d44dbc
Matt <gopezm@myumanitoba.ca>
2018-03-08 03:51:51 -0600

    Removing ?? notation

--- server/db/utils/build_db.js
+++ server/db/utils/build_db.js
@@ -69,15 +69,20 @@ mysql
   .then(conn => {
     console.log(`---Connected to ${MYSQLDB.database} database---`);
     const res = conn.query(
-      'CREATE USER IF NOT EXISTS ??@?? IDENTIFIED BY ?; GRANT ALL PRIVILEGES ON ??.* TO ??@?? WITH GRANT OPTION;',
-      [
-        MYSQLDB.user,
-        MYSQLDB.host,
-        MYSQLDB.password,
-        MYSQLDB.database,
-        MYSQLDB.user,
-        MYSQLDB.host
-      ]
+      'CREATE USER IF NOT EXISTS ' +
+        mysql.escapeId(MYSQLDB.user, true) +
+        '@' +
+        mysql.escapeId(MYSQLDB.host, true) +
+        ' IDENTIFIED BY ' +
+        mysql.escapeId(MYSQLDB.password, true) +
+        '; ' +
+        'GRANT ALL PRIVILEGES ON ' +
+        mysql.escapeId(MYSQLDB.database, true) +
+        '.* TO ' +
+        mysql.escapeId(MYSQLDB.user, true) +
+        '@' +
+        mysql.escapeId(MYSQLDB.host, true) +
+        ' WITH GRANT OPTION;'
     );
     conn.end();
     return res;
