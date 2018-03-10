e747514234e87026b0ee73c50b278816b940fc9f
Raman Maan <maanr@myumanitoba.ca>
2018-03-10 15:53:22 -0600

    Profile now displays name, birthday, and bio
    Also added tests

--- web/src/containers/Profile.jsx
+++ web/src/containers/Profile.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
+import {
+  Container,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Input
+} from 'reactstrap';
 import { fetchProfile, hideProfile } from '../actions';
 
 import './styles/Profile.css';
@@ -8,10 +15,6 @@ import './styles/Profile.css';
 export class Profile extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      modal: false
-    };
 
     this.toggle = this.toggle.bind(this);
   }
@@ -25,20 +28,47 @@ export class Profile extends Component {
     this.props.fetchData();
   }
 
+  renderBody() {
+    return (
+      <div>
+        <div className="name">
+          <h5>Display Name</h5>
+          <Input type="text" defaultValue={this.props.profile.UserName} />
+        </div>
+        <hr />
+        <div className="birthday">
+          <h5>Birthday</h5>
+          <Input
+            type="date"
+            defaultValue={
+              new Date(this.props.profile.Birthday).toJSON().split('T')[0]
+            }
+          />
+        </div>
+        <hr />
+        <div className="bio">
+          <h5>User Bio</h5>
+          <Input type="textarea" defaultValue={this.props.profile.Bio} />
+        </div>
+      </div>
+    );
+  }
+
   render() {
     const modal = !this.props.view ? null : (
       <div className="profile modal" onClick={e => this.toggle(e)}>
-        <div className="modal-dialog">
+        <Container>
           <div className="modal-content" onClick={e => e.stopPropagation()}>
             <ModalHeader toggle={e => this.toggle(e)}>My Profile</ModalHeader>
-            <ModalBody>Hello there</ModalBody>
+            <ModalBody>{this.renderBody()}</ModalBody>
             <ModalFooter>
-              <Button color="primary" onClick={e => this.toggle(e)}>
-                Something
+              <Button color="secondary" outline onClick={e => this.toggle(e)}>
+                Discard Changes
               </Button>
+              <Button color="primary">Save Changes</Button>
             </ModalFooter>
           </div>
-        </div>
+        </Container>
       </div>
     );
 
---
+++ web/src/containers/Profile.test.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import { Profile } from './Profile';
+
+describe('<Profile />', () => {
+  const testProfile = {
+    Bio: 'This is a test bio for a test user',
+    Birthday: '1991-10-18T05:00:00.000Z',
+    UserName: 'Test User'
+  };
+
+  describe('Snapshot tests', () => {
+    it('should be hidden if view is false', () => {
+      const component = renderer.create(<Profile fetchData={jest.fn()} />);
+      expect(component).toMatchSnapshot();
+    });
+
+    it('should display if profile properties are all empty', () => {
+      const component = renderer.create(
+        <Profile
+          view
+          fetchData={jest.fn()}
+          profile={{ Bio: null, Birthday: null, UserName: null }}
+        />
+      );
+      expect(component).toMatchSnapshot();
+    });
+
+    it('should display profile properties by default', () => {
+      const component = renderer.create(
+        <Profile view fetchData={jest.fn()} profile={testProfile} />
+      );
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('Enzyme tests', () => {
+    let wrapper;
+    let mockFetchData;
+    let mockClickHandler;
+
+    beforeEach(() => {
+      mockFetchData = jest.fn();
+      mockClickHandler = jest.fn();
+      wrapper = mount(
+        <Profile
+          profile={testProfile}
+          fetchData={mockFetchData}
+          clickHandler={mockClickHandler}
+        />
+      );
+    });
+
+    it('should fetch data on mount', () => {
+      expect(mockFetchData.mock.calls.length).toBe(1);
+    });
+  });
+});
