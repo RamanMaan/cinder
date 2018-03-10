d209d4b271379b7d20d3924e71775e5b8ca70262
Fujie Chen <imaple7@gmail.com>
2018-03-10 16:57:21 -0600

    Split recommendation part from user endpoint

---
+++ server/_api/__recs.js
@@ -0,0 +1,39 @@
+/**
+ * These are the endpoints for user operations
+ */
+const express = require('express');
+const router = express.Router();
+const recsDB = require('./db/recs');
+const util = require('./util');
+const responses = require('./responses');
+
+router.get('/', (req, res, next) => {
+  const { userID } = req.params;
+  util.validateID(userID);
+
+  return recsDB
+    .getRecs(userID)
+    .then(recs => res.status(responses.SUCCESS).json(recs))
+    .catch(next);
+});
+
+/**
+ * Error handler
+ */
+router.use((err, req, res, next) => {
+  if (err.message.indexOf('[INTERNAL]')) {
+    //eslint-disable-next-line no-console
+    console.error(err.message);
+    return res.status(responses.BAD_REQUEST).send(err.message);
+  }
+
+  next(err);
+});
+
+router.use((err, req, res, next) => {
+  //eslint-disable-next-line no-console
+  console.error(err.message);
+  res.status(responses.SERVER_ERROR).send(err.message);
+});
+
+module.exports = router;
