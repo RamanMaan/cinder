d47257c06ba2011cc8953111374bbaa757dce37b
Raman Maan <maanr@myumanitoba.ca>
2018-03-10 18:31:11 -0600

    Creating a generic filter element

---
+++ web/src/components/FilterElement.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './styles/FilterElement.css';
+
+export default class FilterElement extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      disabled: !this.props.checked
+    };
+  }
+
+  toggle() {
+    this.setState(prevState => {
+      return { disabled: !prevState.disabled };
+    });
+  }
+
+  renderSwitch() {
+    return (
+      <label className="switch">
+        <input
+          type="checkbox"
+          checked={this.props.checked}
+          onChange={() => this.toggle()}
+        />
+        <div className={`slider ${this.props.round ? 'round' : ''}`} />
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <div
+        className={`filter element ${this.state.disabled ? 'disabled' : ''}`}
+      >
+        <div className="toggle">{this.renderSwitch()}</div>
+        <div className="content">{this.props.children}</div>
+      </div>
+    );
+  }
+}
