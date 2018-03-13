e1ec256bdb80adfa7b358161b5b231052e9ae898
edsondc <delacrue@myumanitoba.ca>
2018-03-12 19:42:56 -0500

    Removed unused variable;

--- server/utils/env.js
+++ server/utils/env.js
@@ -1,18 +1,16 @@
 const dotenv = require('dotenv');
 
 const loadEnvFile = () => {
-  var result;
-
   switch((process.env.NODE_ENV || 'development').trim()) {
     case 'production':
-      result = dotenv.config({path: '.env.production'});
+      dotenv.config({path: '.env.production'});
       break;
     case 'test':
-      result = dotenv.config({path: '.env.test'});
+      dotenv.config({path: '.env.test'});
       break;
     case 'development':
     default:
-      result = dotenv.config({path: '.env'});
+      dotenv.config({path: '.env'});
       break;
   }
 };
