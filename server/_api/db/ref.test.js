faf79b8e51ec8254e66735f7385bf91c67b19bb3
Edson Dela Cruz <delacrue@myumanitoba.ca>
2018-03-17 18:10:38 -0500

    Created test for getGender reference function;

---
+++ server/_api/db/ref.test.js
@@ -0,0 +1,18 @@
+/**
+ * Test suite for the functions at ref.js
+ */
+require('dotenv').load();
+const mysql = require('promise-mysql');
+const refDB = require('./ref');
+const refData = require('../../db/referenceData');
+
+it(`returns all available gender reference data`, () => {
+  return refDB.getGender()
+  .then((genderRefs) => {
+    expect(genderRefs).toHaveLength(refData.GenderType.length);
+    genderRefs.forEach((gref, i) => {
+      expect(gref.id).toBe(i + 1);
+      expect(gref.value).toBe(refData.GenderType[i]);
+    });
+  });
+})
