a390351ae520829622c156733878d081ca8d95a5
Min Gyu Park <parkmg@myumanitoba.ca>
2018-03-17 22:37:00 -0500

    Add dummy logout function

--- web/src/utils/authService.js
+++ web/src/utils/authService.js
@@ -1,36 +1,18 @@
 import jwt from 'jsonwebtoken';
 
-class Auth {
-  constructor() {
-    this.token = localStorage.getItem('token') || null;
-    this.userID = this.getUserID(this.token);
+const getAuth = () => {
+  let userID = -1;
+  let token = localStorage.getItem('token') || null;
+  if (token) {
+    const decoded = jwt.decode(token);
+    console.log(decoded);
+    userID = decoded.id;
   }
+  return { token, userID };
+};
 
-  getUserID(token) {
-    let id = -1;
-    if (token) {
-      const decoded = jwt.decode(token);
-      id = decoded.id;
-    }
-    return id;
-  }
+export const logout = () => {
+  console.log('Logging out!');
+};
 
-  getAuth() {
-    const token = this.token;
-    const userID = this.userID;
-    return { token, userID };
-  }
-}
-
-// const getAuth = () => {
-//   let userID = -1;
-//   let token = localStorage.getItem('token') || null;
-//   if (token) {
-//     const decoded = jwt.decode(token);
-//     console.log(decoded);
-//     userID = decoded.id;
-//   }
-//   return { token, userID };
-// };
-
-export default new Auth().getAuth();
+export default getAuth();
