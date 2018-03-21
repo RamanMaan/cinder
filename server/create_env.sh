d4bae46de48e4d591e7d6028599b9ccc38a76953
Matt <gopezm@myumanitoba.ca>
2018-03-20 19:40:55 -0500

    Adding DB_ROOT_USER to AWS env creation script

--- server/create_env.sh
+++ server/create_env.sh
@@ -5,6 +5,7 @@ DB_NAME="$(/opt/elasticbeanstalk/bin/get-config environment -k DB_NAME)"
 
 DB_NAME="$(/opt/elasticbeanstalk/bin/get-config environment -k DB_NAME)"
 DB_PASSWORD="$(/opt/elasticbeanstalk/bin/get-config environment -k DB_PASSWORD)"
+DB_ROOT_USER="$(/opt/elasticbeanstalk/bin/get-config environment -k DB_ROOT_USER)"
 DB_ROOT_PASSWORD="$(/opt/elasticbeanstalk/bin/get-config environment -k DB_ROOT_PASSWORD)"
 DB_USER="$(/opt/elasticbeanstalk/bin/get-config environment -k DB_USER)"
 DB_HOST="$(/opt/elasticbeanstalk/bin/get-config environment -k DB_HOST)"
@@ -28,4 +29,5 @@ DB_PASS=$DB_PASSWORD
 
 # The root password, used in database & user creation
 MYSQL_ROOT_PASS=$DB_ROOT_PASSWORD
+MYSQL_ROOT_USER=$DB_ROOT_USER
 " > .env
