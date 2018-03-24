8ac9cb6de0cba6d837f119aa778dfa25d60f388a
Kevin Dam <kevnis204@Live.ca>
2018-03-23 21:33:52 -0500

    Adding waitForElement for test

--- web/system_tests/approve_user_test.js
+++ web/system_tests/approve_user_test.js
@@ -2,6 +2,7 @@ Feature('Testing approving a recommended user');
 
 Scenario('Test approving a recommended user', I => {
   I.amOnPage('/login');
+  I.waitForElement('#login-form');
   I.see('Log In');
   I.fillField('#email', 'al@email.com');
   I.fillField('#password', 'password');
