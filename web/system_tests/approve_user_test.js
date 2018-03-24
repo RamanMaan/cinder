a661b25debc4af99e954e49c868bef698afa7770
Kevin Dam <kevnis204@Live.ca>
2018-03-23 21:38:04 -0500

    Added time for waitForElement

--- web/system_tests/approve_user_test.js
+++ web/system_tests/approve_user_test.js
@@ -2,14 +2,16 @@ Feature('Testing approving a recommended user');
 
 Scenario('Test approving a recommended user', I => {
   I.amOnPage('/login');
-  I.waitForElement('#login-form');
+  I.waitForElement('.login-form', 2);
   I.see('Log In');
   I.fillField('#email', 'al@email.com');
   I.fillField('#password', 'password');
   I.click('Log In');
   I.amOnPage('/');
+  I.waitForElement('.Home', 2);
   I.see('cinder');
   I.seeElement('.UserDetail');
   I.click('HOT');
+  I.waitForElement('.Home', 2);
   I.seeElement('.UserDetail');
 });
